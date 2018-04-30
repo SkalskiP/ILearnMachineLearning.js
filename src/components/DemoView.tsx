b5b522fcf308f8334acd13d0d939f5cf85512e87
SkalskiP <pskalski@student.agh.edu.pl>
2018-04-30 23:07:54 +0200

    PrettyContainers added

--- src/components/DemoView.tsx
+++ src/components/DemoView.tsx
@@ -5,6 +5,22 @@ import './../styles/DemoView.css';
 export class DemoView extends React.Component {
 
     public render() {
-        return <div className="DemoView"/>;
+
+        return(
+            <div className="DemoView">
+                <div className="PrettyContainer">
+                    <div className="PrettyContainerHeader">
+                        Draw
+                    </div>
+                    <div className="PrettyFiller"/>
+                </div>
+                <div className="PrettyContainer">
+                    <div className="PrettyContainerHeader">
+                        Predictions
+                    </div>
+                    <div className="PrettyFiller"/>
+                </div>
+            </div>
+        );
     }
 }
